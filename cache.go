a5d3d0f51f9c15a9a5043c97f1d82af1ba79a79d
epamanshumishra <anshu_mishra@epam.com>
2022-05-23 12:46:08 +0530

    cache solution

--- cache.go
+++ cache.go
@@ -2,22 +2,60 @@ package cache
 
 import "time"
 
+type item struct {
+	value    string
+	deadline time.Time
+}
+
 type Cache struct {
+	Items map[string]item
 }
 
 func NewCache() Cache {
-	return Cache{}
+	return Cache{Items: map[string]item{}}
 }
 
-func (receiver) Get(key string) (string, bool) {
-
+func (i *item) isExpired() bool {
+	if i.deadline.IsZero() {
+		return false
+	}
+	now := time.Now()
+	return i.deadline.Before(now)
 }
 
-func (receiver) Put(key, value string) {
+func (c Cache) Get(key string) (string, bool) {
+	value, ok := c.Items[key]
+	if !ok {
+		return "", false
+	}
+
+	if value.isExpired() {
+		delete(c.Items, key)
+		return "", false
+	}
+
+	return value.value, true
 }
 
-func (receiver) Keys() []string {
+func (c *Cache) Put(key, value string) {
+	i := item{value: value}
+	c.Items[key] = i
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (c Cache) Keys() []string {
+	keys := make([]string, 0, len(c.Items))
+
+	for k := range c.Items {
+		_, ok := c.Get(k)
+		if ok {
+			keys = append(keys, k)
+		}
+	}
+
+	return keys
+}
+
+func (c *Cache) PutTill(key, value string, deadline time.Time) {
+	i := item{value: value, deadline: deadline}
+	c.Items[key] = i
 }
